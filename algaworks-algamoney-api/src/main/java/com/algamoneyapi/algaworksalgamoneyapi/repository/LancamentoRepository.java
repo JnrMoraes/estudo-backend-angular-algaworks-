95b31a6ee64c30b5e7738f49f574b11a98520161
juniormoraes <contatomoraesjr@gmail.com>
2020-07-21 11:58:28 -0300

    Still trying to make MetaModal works in Intellij

--- algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/repository/LancamentoRepository.java
+++ algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/repository/LancamentoRepository.java
@@ -1,9 +1,10 @@
 package com.algamoneyapi.algaworksalgamoneyapi.repository;
 
 import com.algamoneyapi.algaworksalgamoneyapi.model.Lancamento;
+import com.algamoneyapi.algaworksalgamoneyapi.repository.lancamento.LancamentoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LancamentoRepository extends JpaRepository <Lancamento, Long >  {
+public interface LancamentoRepository extends JpaRepository <Lancamento, Long >, LancamentoRepositoryQuery {
 
 
 }
