51983a1460d7e5e2c690b61f2aa1997f6ca4359b
juniormoraes <contatomoraesjr@gmail.com>
2020-08-14 15:51:52 -0300

    yet not working - add anottation service to AppDetailsService

--- algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/security/AppDetailsService.java
+++ algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/security/AppDetailsService.java
@@ -9,12 +9,14 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+@Service
 public class AppDetailsService implements UserDetailsService {
 
     @Autowired
