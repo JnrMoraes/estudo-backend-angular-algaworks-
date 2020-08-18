886294e779876c13790acb4422c595d0228f7c95
juniormoraes <contatomoraesjr@gmail.com>
2020-08-17 22:07:32 -0300

    configured different profiles in the application-prod

--- algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/AlgaworksAlgamoneyApiApplication.java
+++ algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/AlgaworksAlgamoneyApiApplication.java
@@ -1,9 +1,12 @@
 package com.algamoneyapi.algaworksalgamoneyapi;
 
+import com.algamoneyapi.algaworksalgamoneyapi.config.property.AlgamoneyApiProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgaworksAlgamoneyApiApplication {
 
 	public static void main(String[] args) {
