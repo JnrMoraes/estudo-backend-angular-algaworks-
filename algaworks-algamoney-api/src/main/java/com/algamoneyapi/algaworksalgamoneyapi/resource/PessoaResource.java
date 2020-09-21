19267803aea7151cd62993954eb1ed77cb5e5395
juniormoraes <contatomoraesjr@gmail.com>
2020-09-21 16:12:45 -0300

    7.7 implementado pesquisa de pessoa por paramentro

--- algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/resource/PessoaResource.java
+++ algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/resource/PessoaResource.java
@@ -3,17 +3,27 @@ package com.algamoneyapi.algaworksalgamoneyapi.resource;
 import com.algamoneyapi.algaworksalgamoneyapi.event.RecursoCriadoEvent;
 import com.algamoneyapi.algaworksalgamoneyapi.model.Pessoa;
 import com.algamoneyapi.algaworksalgamoneyapi.repository.PessoaRepository;
+import com.algamoneyapi.algaworksalgamoneyapi.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import com.algamoneyapi.algaworksalgamoneyapi.service.PessoaService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -28,10 +38,10 @@ public class PessoaResource {
     @Autowired
     private PessoaService pessoaService;
 
-    @GetMapping
-    public List<Pessoa>listPessoa(){
-        return pessoaRepository.findAll();
-    }
+//    @GetMapping
+//    public List<Pessoa>listPessoa(){
+//        return pessoaRepository.findAll();
+//    }
 
     @PostMapping
     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
@@ -67,5 +77,11 @@ public class PessoaResource {
     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
     public void updatePropertAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo){
         pessoaService.updatePropertAtivo(codigo,ativo);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+    public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable){
+        return  pessoaRepository.findByNomeContaining(nome, pageable);
     }
 }
