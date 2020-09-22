a6ae105acfbcd25b2dba0f74e3655fb3c7a0f4cc
juniormoraes <contatomoraesjr@gmail.com>
2020-09-22 20:19:15 -0300

    7.9 atulizando metodo lancamento

--- algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/service/LancamentoService.java
+++ algaworks-algamoney-api/src/main/java/com/algamoneyapi/algaworksalgamoneyapi/service/LancamentoService.java
@@ -5,6 +5,7 @@ import com.algamoneyapi.algaworksalgamoneyapi.model.Pessoa;
 import com.algamoneyapi.algaworksalgamoneyapi.repository.LancamentoRepository;
 import com.algamoneyapi.algaworksalgamoneyapi.repository.PessoaRepository;
 import com.algamoneyapi.algaworksalgamoneyapi.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +28,37 @@ public class LancamentoService {
             throw new PessoaInexistenteOuInativaException();
         }
         return lancamentoRepository.save(lancamento);
+    }
+
+    public Lancamento atualizar(Long codigo, Lancamento lancamento){
+        Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+
+        if(!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())){
+            validarPessoa(lancamento);
+        }
+        BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+        return lancamentoRepository.save(lancamentoSalvo);
+    }
+
+    private void validarPessoa(Lancamento lancamento){
+
+        Optional<Pessoa> pessoaOptional = null;
+
+        if (lancamento.getPessoa().getCodigo() != null){
+            pessoaOptional = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+        }
+
+        if (!pessoaOptional.isPresent() || pessoaOptional.isEmpty() || pessoaOptional.get().isInativo()){
+            throw new PessoaInexistenteOuInativaException();
+        }
+
+    }
+
+    private Lancamento buscarLancamentoExistente(Long codigo){
+        Optional<Lancamento> lancamentoOptional = lancamentoRepository.findById(codigo);
+
+        return lancamentoOptional.orElseThrow(IllegalArgumentException::new);
     }
 }
 
